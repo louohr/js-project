b4d9bc7ba869dd56ac67d7d51a5b5160999e35e9
louohr <louise.ohrner@gmail.com>
2024-08-16 14:27:43 +0200

    fix comment

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ searchButton.addEventListener("click", async function () {
     existingSprite.remove();
   }
 
-  // get all pokemons
+  // Get all pokemons
   async function fetchPokemons() {
     let response = await fetch("https://pokeapi.co/api/v2/pokemon/", {
       method: "GET",
